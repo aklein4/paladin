000db4503a93feab2bac8aa3311a2a696c2ad821
Adam Klein <adam3207@gmail.com>
2024-04-10 05:34:22 -0700

    removed dropout

--- train_multipass.py
+++ train_multipass.py
@@ -36,6 +36,10 @@ TRAIN_CONFIG = {
 MODEL_CONFIG = {
     "z_dim": 8,
     "t_dim": 64,
+    "attn_pdrop": 0.0,
+    "embd_pdrop": 0.0,
+    "resid_pdrop": 0.0,
+    "summary_first_dropout": 0.0
 }
 
 
